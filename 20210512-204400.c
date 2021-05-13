a4ed84a3744ed1e1c59992173389c2f6840f8f84
Omar-prog1 <82549988+Omar-prog1@users.noreply.github.com>
2021-05-12 20:46:40 -0500

    Add files via upload

---
+++ 20210512-204400.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    float a, b, c;
+    printf("\n\n\nPrograma ");
+    printf("\n\nExamen Hernández Flores Ricardo Omar 4IV17");
+    printf("\nsuma de enteros");
+    printf("\n\nDame el valor entero de a");
+    scanf("%f",&a);
+    printf("\n\nDame el valor entero de b");
+    scanf("%f",&b);
+    c= a+b;
+    printf("\n El resultado es %f",c);
+    return 0; 
+}
+
