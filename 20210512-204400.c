0ca09c98ab59491710fbf0fea669bcc0666613b2
Omar-prog1 <82549988+Omar-prog1@users.noreply.github.com>
2021-05-12 21:26:14 -0500

    Update 20210512-204400.c

--- 20210512-204400.c
+++ 20210512-204400.c
@@ -3,15 +3,15 @@
 int main()
 {
     float a, b, c;
-    printf("\n\n\nPrograma ");
-    printf("\n\nExamen Hernández Flores Ricardo Omar 4IV17");
-    printf("\nsuma de enteros");
-    printf("\n\nDame el valor entero de a");
-    scanf("%d",&a);
-    printf("\n\nDame el valor entero de b");
-    scanf("%d",&b);
+    printf ("\n\n\nPrograma ");
+    printf ("\n\nExamen Hernández Flores Ricardo Omar 4IV17");
+    printf ("\nsuma de enteros");
+    printf ("\n\nDame el valor entero de a");
+    scanf ("%d",&a);
+    printf ("\n\nDame el valor entero de b");
+    scanf ("%d",&b);
     c= a+b;
-    printf("\n El resultado es %d",c);
+    printf ("\n El resultado es %d",c);
     return 0; 
 }
 
